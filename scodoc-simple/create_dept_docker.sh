0b902f9ec3d4014a1859ad5f904b1e40035e64ac
boudes <boudes@lipn.univ-paris13.fr>
2016-02-14 23:12:58 +0100

    fin adaptation des scripts

--- scodoc-simple/create_dept_docker.sh
+++ scodoc-simple/create_dept_docker.sh
@@ -17,8 +17,8 @@ check_uid_root $0
 
 
 echo -n "Nom du departement (un mot sans ponctuation, exemple \"Info\"): "
-read DEPT
-
+#read DEPT
+DEPT=$1
 if [[ ! "$DEPT" =~ ^[A-Za-z0-9]+$ ]]
 then
  echo 'Nom de departement invalide !'
@@ -41,7 +41,7 @@ fi
 init_postgres_user
 
 # -----------------------  Create database
-su -c ./create_database.sh $POSTGRES_SUPERUSER 
+su -c ./create_database.sh $POSTGRES_SUPERUSER
 
 # ----------------------- Create tables
 # POSTGRES_USER == regular unix user (www-data)
@@ -50,12 +50,16 @@ su -c ./initialize_database.sh $POSTGRES_USER
 # ----------------------- Enregistre fichier config
 echo "dbname="$db_name > $cfg_pathname
 
-# ----------------------- 
+# -----------------------
 echo
 echo " Departement $DEPT cree"
 echo
-echo " Attention: la base de donnees n'a pas de copies de sauvegarde"
-echo 
+echo " mise en place des sauvegardes de la base"
+line1="* * * * * /opt/scodoc/Products/ScoDoc/misc/backup_db2 SCOUSERS"
+line2="* * * * * /opt/scodoc/Products/ScoDoc/misc/backup_db2 SCO${DEPT}"
+(crontab -u postgres -l; echo "${line1}" ) | crontab -u postgres -
+(crontab -u postgres -l; echo "${line2}" ) | crontab -u postgres -
+echo
 echo " Maintenant, vous pouvez ajouter le departement via l'application web"
 echo " en suivant le lien \"Administration de ScoDoc\" sur la page d\'accueil."
 echo
--- scodoc-simple/install_debian7_docker.sh
+++ scodoc-simple/install_debian7_docker.sh
@@ -36,7 +36,7 @@ chgrp -R www-data "${SCODOC_DIR}"/static/photos
 chmod -R g+w "${SCODOC_DIR}"/static/photos
 
 # ------------ LOCALES
-echo 
+echo
 echo '---- Configuration des locales...'
 echo
 
@@ -56,12 +56,12 @@ do
   fi
 done
 
-/usr/sbin/locale-gen --keep-existing 
+/usr/sbin/locale-gen --keep-existing
 
 
 if [ "$LANG" != "en_US.UTF-8" ]
 then
-   # ceci est necessaire a cause de postgresql 8.3 qui 
+   # ceci est necessaire a cause de postgresql 8.3 qui
    # cree son cluster lors de l'install avec la locale par defaut !
    echo "Attention: changement de la locale par defaut"
    mv /etc/default/locale /etc/default/locale.orig
@@ -74,16 +74,15 @@ echo 'Done.'
 # ------------ AJOUT DES PAQUETS NECESSAIRES
 apt-get update
 apt-get -y install subversion curl cracklib-runtime firehol
-apt-get -y install apache2 ssl-cert 
 apt-get -y install postgresql-9.1 postgresql-client-9.1
 apt-get -y install graphviz
 
 # ------------ INSTALL DES EXTENSIONS PYTHON (2.7)
 
-apt-get -y install python-jaxml 
-apt-get -y install python-psycopg2 
-apt-get -y install python-pyrss2gen 
-apt-get -y install python-imaging python-reportlab 
+apt-get -y install python-jaxml
+apt-get -y install python-psycopg2
+apt-get -y install python-pyrss2gen
+apt-get -y install python-imaging python-reportlab
 apt-get -y install python-cracklib # was python-crack
 apt-get -y install python-pyparsing
 apt-get -y install python-beautifulsoup
@@ -121,117 +120,12 @@ echo
 
 echo 'Done.'
 
-# ------------ POSTFIX
-echo 
-echo "ScoDoc a besoin de pouvoir envoyer des messages par mail."
-echo -n "Voulez vous configurer la messagerie (tres recommande) ? (y/n) [y] "
-read ans
-if [ "$(norm_ans "$ans")" != 'N' ]
-then
-    apt-get -y install postfix
-fi
-
-# ------------ CONFIG FIREWALL
-echo 
-echo "Le firewall aide a proteger votre serveur d'intrusions indesirables."
-echo -n "Voulez vous installer un firewall minimal ? (y/n) [n] "
-read ans
-if [ "$(norm_ans "$ans")" = 'Y' ]
-then
-    echo 'Installation du firewall IP (voir /etc/firehol/firehol.conf)'
-    echo "Attention: suppose que l'interface reseau vers Internet est eth0"
-    echo "  si ce n'est pas le cas, editer /etc/firehol/firehol.conf"
-    echo "  et relancer: /etc/init.d/firehol restart"
-    echo
-    cp $SCODOC_DIR/config/etc/firehol.conf /etc/firehol/
-    mv /etc/default/firehol /etc/default/firehol.orig
-    cat /etc/default/firehol.orig | sed 's/START_FIREHOL=NO/START_FIREHOL=YES/' > /tmp/firehol && mv /tmp/firehol /etc/default/firehol
-    # mise a jour RESERVED_IPS (le script fournit en Deb 7 get-iana est buggy)
-    echo yes |  $SCODOC_DIR/config/softs/get-iana.sh
-fi
-
-# Nota: after this point, the network may be unreachable 
-# (if firewall config is wrong)
-
-# ------------ CONFIG APACHE
-a2enmod ssl
-a2enmod proxy
-a2enmod proxy_http
-a2enmod rewrite
-
-echo 
-echo "La configuration du serveur web va modifier votre installation Apache pour supporter ScoDoc."
-echo -n "Voulez vous configurer le serveur web Apache maintenant ? (y/n) [y] "
-read ans
-if [ "$(norm_ans "$ans")" != 'N' ]
-then
-    echo "Configuration d'Apache"
-    server_name=""
-    while [ -z $server_name ]
-    do
-        echo "Le nom de votre serveur doit normalement etre connu dans le DNS."
-	echo -n "Nom complet de votre serveur (exemple: notes.univ.fr): "
-	read server_name
-    done
-    # --- CERTIFICATS AUTO-SIGNES
-    echo 
-    echo "Il est possible d'utiliser des certificats cryptographiques"
-    echo "auto-signes, qui ne seront pas reconnus comme de confiance"
-    echo "par les navigateurs, mais offrent une certaine securite."
-    echo -n 'Voulez vous generer des certificats ssl auto-signes ? (y/n) [y] '
-    read ans
-    if [ "$(norm_ans "$ans")" != 'N' ]
-    then
-        # attention: utilise dans scodoc-site-ssl.orig
-	    ssl_dir=/etc/apache2/scodoc-ssl 
-    	if [ ! -e $ssl_dir ]
-	    then
-          mkdir $ssl_dir
-	    fi
-	    /usr/sbin/make-ssl-cert /usr/share/ssl-cert/ssleay.cnf $ssl_dir/apache.pem
-    fi
-    # ---
-    echo 'generation de /etc/apache2/sites-available/scodoc-site-ssl'
-    cat $SCODOC_DIR/config/etc/scodoc-site-ssl.orig | sed -e "s:YOUR\.FULL\.HOST\.NAME:$server_name:g" > /etc/apache2/sites-available/scodoc-site-ssl
-    echo 'activation du site...'
-    a2ensite scodoc-site-ssl
-
-    echo 'Remplacement du site Apache par defaut (sic ! old saved as .bak)'
-    fn=/etc/apache2/sites-available/default
-    if [ -e $fn ]
-    then
-       mv $fn $fn.bak
-    fi
-    cp $SCODOC_DIR/config/etc/scodoc-site.orig $fn
-
-    if [ -z "$(grep Listen /etc/apache2/ports.conf | grep 443)" ]
-    then
-      echo 'adding port 443'
-      echo 'Listen 443' >> /etc/apache2/ports.conf
-    fi
-
-    echo 'configuring Apache proxy'
-    mv /etc/apache2/mods-available/proxy.conf /etc/apache2/mods-available/proxy.conf.bak
-    cat > /etc/apache2/mods-available/proxy.conf <<EOF
-<IfModule mod_proxy.c>
-# Proxy config for ScoDoc default installation
-ProxyRequests Off
-  <ProxyMatch http://localhost:8080>
-          Order deny,allow
-          Allow from all
-  </ProxyMatch>
-</IfModule>
-EOF
-
-    /etc/init.d/apache2 restart
-fi
-
-
 # ------------ CONFIG SERVICE SCODOC
-echo 
+echo
 echo "Installer le service scodoc permet de lancer automatiquement le serveur au demarrage."
-echo -n "Voulez vous installer le service scodoc ? (y/n) [y] "
-read ans
+#echo -n "Voulez vous installer le service scodoc ? (y/n) [y] "
+#read ans
+ans='Y'
 if [ "$(norm_ans "$ans")" != 'N' ]
 then
     echo 'Installation du demarrage automatique de ScoDoc'
@@ -248,5 +142,3 @@ echo "puis creer un departement avec  ./create_dept.sh"
 echo "Ou bien restaurer vos donnees a partir d'une ancienne installation a l'aide du script restore_scodoc_data.sh"
 echo "(voir https://trac.lipn.univ-paris13.fr/projects/scodoc/wiki/MigrationVersScoDocSept)"
 echo
-
-
